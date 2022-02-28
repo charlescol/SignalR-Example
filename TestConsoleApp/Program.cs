5b1418c2b71d1682fc96c70905359bcb8fae3d87
charlescol <76974177+charlescol@users.noreply.github.com>
2022-02-28 10:46:21 -0500

    Update Program.cs

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System; 
+using Microsoft.AspNetCore.SignalR.Client;
+using Newtonsoft.Json;
 
-namespace TestConsoleApp
+namespace ConsoleApp1
 {
+    using MessageItem = AppModel.Storage.ReferencedItem<System.Guid, AppModel.Message.Message<System.Guid>>;
     class Program
     {
         static void Main(string[] args)
         {
+            HubConnection connection = new HubConnectionBuilder().WithUrl("http://localhost:7075/api", 
+                (option) => option.Headers.Add("x-ms-signalr-userid", "cd1b2d6a-46a0-4a5c-a489-2cee68f75b6d")).Build();
+            connection.On<string>("MessageSent", ShowMessage);
+            connection.StartAsync();
+            
+            Console.ReadLine();
+        }
+        public static void ShowMessage(string content)
+        {
+            try
+            {
+                var message = JsonConvert.DeserializeObject<MessageItem>(content);
+                Console.WriteLine(message.Item.TextContent);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
